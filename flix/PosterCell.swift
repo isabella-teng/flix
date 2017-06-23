4161a452caa64e33a3c1c9140af8007225498864
Isabella Teng <isabellateng@fb.com>
2017-06-23 16:00:58 -0700

    Added collection view and ratings to detail view

---
+++ flix/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Isabella Teng on 6/23/17.
+//  Copyright © 2017 Isabella Teng. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
